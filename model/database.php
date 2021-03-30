b26b2fe1addf21b148c056719ac421f21f0032fe
Rebekah <rebekah.kivett@gmail.com>
2021-03-30 14:01:46 -0500

    fix password param

--- model/database.php
+++ model/database.php
@@ -2,6 +2,7 @@
     $dsn = 'mysql:host=xlf3ljx3beaucz9x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=dk3gu6vztlu6z6ncgit';
     $username = 'gwof063yrr7icqb7';
     $password = 's5wnc12xrptqdc5h';
+    
 
     // Heroku connection
     /* 
@@ -12,7 +13,7 @@
     try {
         //local development server connection
         //if using a $password, add it as 3rd parameter
-        $db = new PDO($dsn, $username);
+        $db = new PDO($dsn, $username, $password);
 
         // Heroku connection
         //$db = new PDO($dsn, $username, $password);
