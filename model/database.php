c8bd70d92f78efe46160bcd06c8837eae30dadb2
Rebekah <rebekah.kivett@gmail.com>
2021-04-18 15:25:29 -0500

    Left out

--- model/database.php
+++ model/database.php
@@ -5,6 +5,7 @@ class Database {
 private static $dsn = 'mysql:host=u6354r3es4optspf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=ijirs3sxwmlg17ox';
 private static $username = 'ykxvv83rr9wo0imm';
 private static $password= 'ofiehgihgtoorgr3';
+private static $db;
 
 //for local use
 //private static $dsn = 'mysql:host=localhost;dbname=zippyusedautos';
